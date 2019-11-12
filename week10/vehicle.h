ae7865102fa5c22c06eb34f0af6c2759fee122cb
HwangGyujin <rbwls460@naver.com>
2019-11-12 14:52:02 +0900

    week10

---
+++ week10/vehicle.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class vehicle {
+public :
+	vehicle();
+	vehicle(int wheel_number,int max_speed);
+	vehicle(int wheel_number, int max_speed, bool has_name);
+	bool has_name;
+	int wheel_number;
+	int max_speed;
+	int get_wheel_number();
+	int get_max_speed();
+	bool get_has_name();
+	const char* get_class_name();
+	void set_wheel_number(int wheel_number);
+	void set_max_speed(int max_speed);
+	void set_has_name(bool has_name);
+};
